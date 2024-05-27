989492423984838cbb33c4e7a168bd0852787abd
github_gilivex <gilivex@gmail.com>
2024-05-27 10:03:54 +0300

    my_transaction

--- alu_mem/my_transaction.sv
+++ alu_mem/my_transaction.sv
@@ -1,27 +1,27 @@
 // calss transaction uvm sqeuence item
 
 parameter int ADDAR_WIDTH = 2;
+parameter int DATA_WIDTH = 8;
 
 class my_transaction extends uvm_sequence_item;
- 
+    // input port
+   
+    //Address for writing or reading
+    rand bit [ADDAR_WIDTH-1:0] addr;
+    //write data drive by master
     rand bit wr_data;
-    rand bit read_data;
-    //constraint for write_en and read_en they 
-    //should not be high at the same time but both could be low
-    constraint c_write_read { !(write_en && read_en); }
+    // 0-write, 1-read
+    rand bit rd_wr;
+    //enable for write or read
+    rand bit enable;
 
-    rand bit [3:0] data_in;
 
-    // out put port 
-    bit full = 0;
-    bit empty = 1;
-    bit [3:0] data_out;
-    bit rst = 0;
+    // output port
+    //read data drive by slave
+    bit [DATA_WIDTH-1:0] rd_data;
+   //output result
+    bit [16-1:0] res_out = 0;
     
-    // bit to determine if the transaction data is valid or not 
-    // we use it in the scoreboard and the reference model
-    bit is_data_valid = 0;
-
 
     // `uvm_object_utils(my_transaction);
 
@@ -30,13 +30,12 @@ class my_transaction extends uvm_sequence_item;
     endfunction
 
     `uvm_object_utils_begin(my_transaction)
-        `uvm_field_int(rst, UVM_ALL_ON);
-        `uvm_field_int(write_en, UVM_ALL_ON);
-        `uvm_field_int(read_en, UVM_ALL_ON);
-        `uvm_field_int(data_in, UVM_ALL_ON);
-        `uvm_field_int(full, UVM_ALL_ON);
-        `uvm_field_int(empty, UVM_ALL_ON);
-        `uvm_field_int(data_out, UVM_ALL_ON);
+        `uvm_field_int(addr, UVM_ALL_ON)
+        `uvm_field_int(wr_data, UVM_ALL_ON)
+        `uvm_field_int(rd_wr, UVM_ALL_ON)
+        `uvm_field_int(enable, UVM_ALL_ON)
+        `uvm_field_int(rd_data, UVM_ALL_ON)
+        `uvm_field_int(res_out, UVM_ALL_ON)
 
    `uvm_object_utils_end
 
