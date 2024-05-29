647c7ff315fbbe4befd57eca9225faf56d023ff1
github_gilivex <gilivex@gmail.com>
2024-05-29 22:33:09 +0300

    test

--- alu_mem/my_sequence.sv
+++ alu_mem/my_sequence.sv
@@ -8,15 +8,15 @@ class my_sequence extends uvm_sequence #(my_transaction);
 
     task body();
 
-        // init the reg model
-        for(int i = 0; i < 10; i++) begin
-            req = my_transaction::type_id::create("req");
-            start_item(req);
-            req.addr = i%4;
-            req.wr_data = 3;
-            req.rd_wr = 0;
-            finish_item(req);
-        end
+        // // init the reg model
+        // for(int i = 0; i < 10; i++) begin
+        //     req = my_transaction::type_id::create("req");
+        //     start_item(req);
+        //     req.addr = i%4;
+        //     req.wr_data = 3;
+        //     req.rd_wr = 0;
+        //     finish_item(req);
+        // end
 
         repeat(200) begin
             // start_item() is a method of uvm_sequence that creates a new transaction
